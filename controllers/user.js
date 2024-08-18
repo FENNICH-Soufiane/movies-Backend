de262883fd9339f41576cc2843c8402868464323
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2024-08-18 18:17:47 +0100

    sending otp verification email

--- controllers/user.js
+++ controllers/user.js
@@ -1,28 +1,58 @@
+const emailVerificationToken = require('../models/emailVerificationTokenSchema');
 const User = require('../models/user');
-
+const nodemailer = require('nodemailer')
 
 exports.create = async (req, res) => {
   try {
     const { name, email, password } = req.body;
-    const oldUser = await User.findOne({email});
-    
-    if(oldUser) {
-      return res.status(401).json({error: "This email is already in use !"});
+    const oldUser = await User.findOne({ email });
+
+    if (oldUser) {
+      return res.status(401).json({ error: "This email is already in use !" });
     }
-    const newUser = new User({ name, email, password })
-    await newUser.save()
+    const newUser = new User({ name, email, password });
+    await newUser.save();
 
+
+    // generate 6 digit otp
+    let OTP = '';
+    for (let i = 0; i <= 5; i++) {
+      const randomVal = Math.round(Math.random() * 9)
+      OTP += randomVal;
+    }
+
+    // store otp inside in db
+    const newEmailVerificationToken = new emailVerificationToken({ owner: newUser._id, token: OTP });
+
+    await newEmailVerificationToken.save();
+
+    // send that otp to user
     var transport = nodemailer.createTransport({
-
-      host: "sandbox.smtp.mailtrap.io",    
-      port: 2525,    
+      host: "sandbox.smtp.mailtrap.io",
+      port: 2525,
       auth: {
         user: "9c6a9026369578",
         pass: process.env.pass_nodemailer
       }
     });
 
-    res.status(201).json({ user: newUser })
+    let mailOptions = {
+      from: 'fennich0011soufiane@gmail.com',
+      to: newUser.email,
+      subject: 'Email Verification',
+      html: `
+        <p>You verification OTP</p>
+        <h1>${OTP}</h1>
+      `
+    };
+
+    transport.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        return res.status(500).send({ message: 'Erreur lors de l\'envoi de l\'email' });
+      }
+      res.status(201).json({ message: 'Please verify you email. OTP has been sent to your email account!' });
+    });
+
   } catch (error) {
     res.status(500).json({ error })
   }
