7bdf17669e2379e738e0fc04036e1404414e2a61
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2024-08-25 13:55:55 +0100

    Fixing 404 Not Found

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require("express");
 require('./db')
 const userRouter = require("./routes/user");
+
 const cors = require("cors");
+const { handleNotFound } = require("./utils/helper");
+const { errorHandler } = require("./middlewares/error");
 
 const app = express();
 app.use(cors());
 app.use(express.json())
 app.use("/api/user", userRouter);
-
+app.use("/*", handleNotFound);
+app.use(errorHandler);
 
 const port = process.env.port || 5000
 app.listen(port, () => {
--- utils/helper.js
+++ utils/helper.js
@@ -14,3 +14,8 @@ exports.generateRandomByte = () => {
 exports.sendError = (res, error, statusCode = 401) => (
    res.status(statusCode).json({ error })
 )
+
+
+exports.handleNotFound = (req, res) => {
+   this.sendError(res, "Not found", 404);
+ };
