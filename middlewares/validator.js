7844698a4df2ab80caedffaf643f3c8cff2582ed
FENNICH-Soufiane <fennich0011soufiane@gmail.com>
2024-08-27 19:50:47 +0100

    Add SignIn Controller & Validator

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -22,6 +22,26 @@ exports.validatePassword = [
         .withMessage("Password must be 8 to 20 characters long!"),
 ];
 
+exports.signInValidator = [
+    check("email").normalizeEmail().isEmail().withMessage("Email is invalid!"),
+    check("password").trim().not().isEmpty().withMessage("Password is missing!"),
+  ];
+  
+  exports.actorInfoValidator = [
+    check("name").trim().not().isEmpty().withMessage("Actor name is missing!"),
+    check("about")
+      .trim()
+      .not()
+      .isEmpty()
+      .withMessage("About is a required field!"),
+    check("gender")
+      .trim()
+      .not()
+      .isEmpty()
+      .withMessage("Gender is a required field!"),
+  ];
+  
+
 exports.validate = (req, res, next) => {
     const error = validationResult(req).array();
     if (error.length) {
